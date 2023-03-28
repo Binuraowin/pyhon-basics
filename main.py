6dbea846fd38126ac7e3779b1931e7162c679f6c
binura-owin <binura.owin1@gmail.com>
2023-03-28 23:06:15 +0530

    else

--- main.py
+++ main.py
@@ -2,4 +2,21 @@
 # school = input ("enter your school: ")
 # print("hi",name, "your school is", school) 
 
-print(type(2.88888))
+# print(type(2.88888))
+
+# length = input("Enter length: ")
+# width = input("enter width: ")
+
+# if(length == width):
+#     print("square")
+# else:
+#     print("traingle")
+
+year = int(input("enter your age: "))
+
+age = 2023 - year
+
+if(age>18):
+    print("adult")
+else:
+    print("child")
